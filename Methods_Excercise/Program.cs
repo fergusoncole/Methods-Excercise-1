f7768ff4256340d11d9e49c44eb2fde5c08dac02
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-05 10:59:56 -0500

    Initial commit on methods excercise 1

---
+++ Methods_Excercise/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What is your name?"); //output
+var name = Console.ReadLine();
+Console.WriteLine("What is your favorite color?"); //output
+var color = Console.ReadLine();
+Console.WriteLine("What is your favorite song to party to?"); //output
+var song = Console.ReadLine();
+Console.WriteLine("What's your favorite dance move?"); //output
+var move = Console.ReadLine();
+Console.WriteLine($"Hello {name}, I just wanted to say that {color} shirt looked rad at the party.");
+Console.WriteLine($"When {song} started playing, and you started busting a move...");
+Console.WriteLine($"I think it was the {move} it took everyones breath away!");
